dc6dd2b178c5d5e1d08f0bef94b59f569c731f00
Jonathan Oribello <joribello@oberyn.lbl.gov>
2021-01-19 10:04:54 -0800

    Added framework for V-V_Program including raw read V-V

---
+++ V-V_Program.py
@@ -0,0 +1,31 @@
+""" Validation and Verification Program For RNASeq Consenus Pipeline
+
+
+"""
+import os
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+from lib import raw_reads
+
+# TODO: convert to proper configuration
+GLDS = 194
+PARENT_PATH = "/data2/JO_Internship_2021/V-V_scripts"
+
+DATA_PATH = os.path.join(PARENT_PATH, f"GLDS-{GLDS}")
+PAIRED_END = True
+
+def main():
+    """ Calls raw and processed data V-V functions
+    """
+    log.info("Starting Raw Data V-V")
+    raw_path = os.path.join(DATA_PATH, "00-RawData", "Fastq")
+    log.debug(f"raw_path: {raw_path}")
+    raw_results = raw_reads.VV(input_path=raw_path,
+                               paired_end=PAIRED_END)
+    log.info("Finished Raw Data V-V")
+
+if __name__ == '__main__':
+    main()
