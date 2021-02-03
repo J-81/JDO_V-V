53c7c4dc0241c508eb75c15e93cc5490cbc60fa0
J-81 <jonathan.d.oribello@gmail.com>
2021-02-03 00:53:47 -0800

    removed unused utility function imported.

--- VV/star.py
+++ VV/star.py
@@ -5,7 +5,7 @@ import subprocess
 import logging
 log = logging.getLogger(__name__)
 
-from VV.utils import bytes_to_gb, sampleWise_outlier_check, max_median_mean_stdev
+from VV.utils import sampleWise_outlier_check, max_median_mean_stdev
 
 class StarAlignments():
     """ Representation of Star Alignment output results data.
