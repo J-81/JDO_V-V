fcd11a645dfbdf1181ceed25ef654b3706d186a1
J-81 <jonathan.d.oribello@gmail.com>
2021-03-13 13:27:54 -0800

    Updated flagging. Now logs entity and sets halt level by argument rather than config parsing.

--- VV/flagging.py
+++ VV/flagging.py
@@ -3,61 +3,51 @@ the V-V checks.  Uses logging
 """
 from datetime import datetime
 import sys
-import argparse
-import configparser
 
-def _parse_args():
-    """ Parse command line args.
-    """
-    parser = argparse.ArgumentParser(description='Perform Automated V&V on '
-                                                 'raw reads.')
-    parser.add_argument('--config', metavar='c', nargs='+', required=True,
-                        help='INI format configuration file')
+FLAG_LEVELS = {
+    20:"Info-Only",
+    30:"Passed-Green",
+    50:"Warning-Yellow",
+    60:"Warning-Red",
+    90:"Issue-Halt_Processing"
+    }
 
-    args = parser.parse_args()
-    print(args)
-    return args
-
-
-args = _parse_args()
-config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
-config.read(args.config)
+class VVError(Exception):
+    pass
 
 class Flagger():
     """ Flagging object
     """
-    def __init__(self, script):
+    def __init__(self,
+                 script: str,
+                 halt_level: int,
+                 step: str = "General VV",):
         self._script = script # location of flagging script
-        self._severity = {  20:"Info",
-                            30:"Anamoly",
-                            50:"Yellow Warning",
-                            70:"Red Warning",
-                            90:"Fail"}
+        self._step = step # location of flagging script
+        self._severity = FLAG_LEVELS
+        self._halt_level = halt_level # level to raise a VV exception at
         self._log_threshold = 30
         timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
-        self._log_file = f"{timestamp}_VV_Results.txt"
+        self._log_file = f"{timestamp}_VV_Results.tsv"
         with open(self._log_file, "w") as f:
-            f.write("START OF VV RUN:\n")
-            f.write(f"Time started: {timestamp}\n")
-            f.write(f"Python Command: {' '.join(sys.argv)}\n")
+            f.write("#START OF VV RUN:\n")
+            f.write(f"#Time started: {timestamp}\n")
+            f.write(f"#Python Command: {' '.join(sys.argv)}\n")
 
-    def flag(self, message, severity, checkID):
+    def set_step(self, step: str):
+        self._step = step
+
+    def set_script(self, script: str):
+        self._script = script
+
+    def flag(self, entity: str, message: str, severity: int, checkID: str):
         """ Given an issue, logs a flag, prints human readable message
         """
-        report = f"{self._severity[severity]}: {message}: source:{self._script}: checkID: {checkID}"
+        report = f"{self._severity[severity]}\t{self._step}\t{self._script}\t{entity}\t{message}\t{checkID}"
         print(report)
         with open(self._log_file, "a") as f:
             f.write(report + "\n")
 
         # full exit upon severe enough issue
-        if severity >= config["Logging"].getint("HaltSeverity"):
-            print("SEVERE ISSUE, HALTING V-V AND ANY ADDITIONAL PROCESSING")
-            sys.exit()
-
-class FlaggedEntity():
-    """ Representation of flaggable entities.
-
-    Unique indentifier format:
-    DATASET:SAMPLES:READSET:READS:POSITIONS
-
-    """
+        if severity >= self._halt_level:
+            raise VVError("SEVERE ISSUE, HALTING V-V AND ANY ADDITIONAL PROCESSING")
