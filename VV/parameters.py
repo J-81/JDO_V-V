ea868f2e4b51ea73f8eb62e795ab9ef798a0ab1f
J-81 <jonathan.d.oribello@gmail.com>
2021-03-13 19:46:00 -0800

    Added new params. Also changed order of threshold entries. This will facilitate sorting thresholds so the most severe is checked first.  In general, once a threshold is tripped, the less severe threshold are not checked (they would also trip).

--- VV/parameters.py
+++ VV/parameters.py
@@ -1,18 +1,27 @@
 """ Stores configuration parameters for VV in python
 
     Top level objects should include all parameters for an entire VV process
-    Final parameters must be in the following format: {FLAG_LEVEL : value}
+    Final parameters must be in the following format: {value: FLAG_LEVEL}
     Empty dicts should be used to explicitly indicate no checks
 """
 DEFAULT_PARAMS = \
 {
+    "middlepoint": "median",
     "raw_reads": {
         "fastq_lines_to_check" : 10000,
         "sequence_length" : {
             "max_thresholds" : {},
             "min_thresholds" : {},
             "outlier_thresholds" : {
-                60 : 0,
+                0 : 60,
+            },
+        },
+        "file_size" : {
+            "max_thresholds" : {},
+            "min_thresholds" : {},
+            "outlier_thresholds" : {
+                2 : 50,
+                4 : 60,
             },
         },
         "percent_duplicates" :{
