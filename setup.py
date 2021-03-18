d2170ed3cb455aab52661094f27a552efae6a195
J-81 <jonathan.d.oribello@gmail.com>
2021-03-17 22:35:06 -0700

    Updated to dev version. This should be done directly after adding first changes from previous version.  Dev tag to be removed once progress to 0.1.3 is complete and tested.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='VV',
-   version='0.1.2',
+   version='0.1.3-dev',
    description='VV for RNASeq raw and processed data.',
    author='Jonathan Oribello',
    author_email='jonathan.d.oribello@gmail.com',
