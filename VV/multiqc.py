8ddb8e0a1aab45dd55618c152ebc2db34476a8e5
J-81 <jonathan.d.oribello@gmail.com>
2021-03-26 15:00:36 -0700

    Reworked check to check for sample name in file. This is less strict than the previous check but addresses the issue of file names being changed for storage AFTER running multiqc with original names.

--- VV/multiqc.py
+++ VV/multiqc.py
@@ -162,7 +162,7 @@ class MultiQC():
             for filelabel, search_file in file_map.items():
                 # search_file: # has extension and parent paths, these are removed when comparing
                 # query_filename: sample1_R1 # notice no extension
-                if search_file.with_suffix("").with_suffix("").name ==  query_filename:
+                if query_filename in search_file:
                     if not matched:
                         matched = (sample, filelabel)
                     else:
