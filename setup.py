573a3cafedce9ed5ad1a735cabb06e87a315c55c
J-81 <jonathan.d.oribello@gmail.com>
2021-05-11 10:53:17 -0700

    Updated to version 0.4.0. Dev status will no longer be implied in the version.  Instead this status will be managed at the git level.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
    name='VV',
-   version='0.4.9-dev',
+   version='0.4.0',
    description='VV for RNASeq raw and processed data.',
    author='Jonathan Oribello',
    author_email='jonathan.d.oribello@gmail.com',
