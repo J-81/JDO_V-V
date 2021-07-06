bd45eb0ae5803bfcea390484580d765d45ce0775
J-81 <jonathan.d.oribello@gmail.com>
2021-07-06 15:23:21 -0700

    Updated version for lbl testing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
    name='VV',
-   version='0.5.1',
+   version='0.4.1d',
    description='VV for raw and processed data. Includes RNASeq and Microarray',
    author='Jonathan Oribello',
    author_email='jonathan.d.oribello@gmail.com',
