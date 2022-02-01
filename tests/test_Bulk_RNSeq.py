4cdac72ab3f8798d105d0a69358539d201f790a7
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-02-01 12:38:42 -0800

    Updated paths, removed hard coding test assets

--- tests/test_Bulk_RNSeq.py
+++ tests/test_Bulk_RNSeq.py
@@ -1,12 +1,44 @@
+import os
+from pathlib import Path
 
-from VV.microarray.file_search import main
+from VV.file_search import path_annotate, get_listing
+
+from VV import generate_publish_to_repo_excel
+
+try:
+    TEST_ASSETS_DIR = os.environ["PTW_TEST_ASSETS"]
+except KeyError as e:
+    print("PTW user needs to set env variable: 'PTW_TEST_ASSETS' to indicate where test assets are stored")
+    raise e
+
+def test_get_listing():
+    """ Function to get a dataframe of all files and directories in a root directory """
+    target_dir = Path(f"{TEST_ASSETS_DIR}/GLDS-123")
+    df_listing = get_listing(target_dir)
+    
+    assert len(df_listing) == 26
+    assert set(df_listing.columns) == {'fullPath','relativePath','annotations','pathObj','isDir'}
+
+def test_GLDS_194():
+    config_fs_f = "/global/scratch/joribell/local_repos/JDO_V-V/VV/RNASeq/Bulk_PI_Search_Patterns.yaml" 
+    runsheet = Path(f"{TEST_ASSETS_DIR}/GLDS-194/Metadata/AST_autogen_template_RNASeq_RCP_GLDS-194_RNASeq_runsheet.csv")
+    root_dir = Path(f"{TEST_ASSETS_DIR}/GLDS-194") 
+
+    result = path_annotate(config_fs_f, runsheet, platform='Bulk_RNASeq:PairedEnd', root_dir=root_dir)
+    
+    assert len(result) == 657
+    assert set(result.columns) == {'annotations', 'pathObj', 'Excel Column', 'relativePath', 'Excel File', 'isDir', 'Excel Sheet', 'fullPath', 'search_pattern'}
+
+def test_generate_publish_to_repo_excel():
+    config_fs_f = "/global/scratch/joribell/local_repos/JDO_V-V/VV/RNASeq/Bulk_PI_Search_Patterns.yaml" 
+    runsheet = Path(f"{TEST_ASSETS_DIR}/GLDS-194/Metadata/AST_autogen_template_RNASeq_RCP_GLDS-194_RNASeq_runsheet.csv")
+    root_dir = Path(f"{TEST_ASSETS_DIR}/GLDS-194") 
+
+    generate_publish_to_repo_excel.main(root_dir, runsheet, template="Bulk_RNASeq:PairedEnd", config_fs_f=config_fs_f)
+
 
 def test_GLDS_123():
-    config_fs_f = "/global/scratch/joribell/local_repos/JDO_V-V/VV/microarray/file_patterns.yaml" 
-    runsheet = "/global/scratch/DP_Pipeline_Drafts/Microarray_dev_20220107/dev_version1_20220107/GLDS-123/Metadata/AST_autogen_template_Microarray_GLDS-123_a_e-mtab-3289_transcription_profiling_DNA_microarray_proto_run_sheet.csv"
-
-    result = main(config_fs_f, runsheet, platform='agilent:two_channel')
-    assert result == 1
+    ...
 
 def test_GLDS_1():
     ...
