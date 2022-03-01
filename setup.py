4b8c1c99347b6863931de0e4b6a02c4d7f6aaa3c
Jonathan Oribello <jonathan.d.oribello@nasa.gov>
2022-03-01 11:16:16 -0800

    Black linter reformat

--- setup.py
+++ setup.py
@@ -5,22 +5,21 @@ from setuptools import setup, find_packages
 dirname = os.path.dirname(__file__)
 
 long_description = (
-    codecs.open(os.path.join(dirname, 'README.md'), encoding='utf-8').read() + '\n' +
-    codecs.open(os.path.join(dirname, 'CHANGELOG.md'), encoding='utf-8').read()
+    codecs.open(os.path.join(dirname, "README.md"), encoding="utf-8").read()
+    + "\n"
+    + codecs.open(os.path.join(dirname, "CHANGELOG.md"), encoding="utf-8").read()
 )
 
 setup(
-   name='gl4gl',
-   version='0.6.0',
-   description='Tools for GeneLab supporting data processing',
-   author='Jonathan Oribello',
-   author_email='jonathan.d.oribello@gmail.com',
-   packages=find_packages(),  #same as name
-   scripts=[
-            'scripts/V-V_Program',
-           ],
-   python_requires='>=3.8',
-   install_requires=['pandas','isatools'],
-   setup_requires=['pytest-runner'],
-   tests_require=['pytest']
+    name="gl4gl",
+    version="0.6.0",
+    description="Tools for GeneLab supporting data processing",
+    author="Jonathan Oribello",
+    author_email="jonathan.d.oribello@gmail.com",
+    packages=find_packages(),  # same as name
+    scripts=["scripts/V-V_Program",],
+    python_requires=">=3.8",
+    install_requires=["pandas", "isatools", "multiqc==1.11"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 )
